45648672f33e9d1b9e54862b7088516caaf5e6db
Gustavo Pavanin Martins <64648417+GustavoPavanin@users.noreply.github.com>
2022-02-11 20:03:17 -0300

    servlets

---
+++ gerenciador/src/main/java/br/com/alura/gerenciador/servlet/OiMundoServlet.java
@@ -0,0 +1,30 @@
+package br.com.alura.gerenciador.servlet;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+
+@WebServlet(urlPatterns = "/oi")
+public class OiMundoServlet extends HttpServlet{
+	
+	@Override
+	protected void service(HttpServletRequest request, HttpServletResponse response) throws IOException{
+		
+		 PrintWriter out = response.getWriter();
+		 out.println("<html>");
+		 out.println("<body>");
+		 out.println("<h1>Olá, mundo! Servlet funcionando normalmente!</h1>");
+		 out.println("</body>");
+		 out.println("</html>");
+		
+		 System.out.println("O Servlet OiMundoServlet foi chamado.");
+	}
+	
+
+}
